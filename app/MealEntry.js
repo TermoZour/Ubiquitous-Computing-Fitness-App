8ab3288edcb796025df7ef09a9456e70a831d0c6
TermoZour <stefanpruneanu@gmail.com>
2023-01-09 13:18:29 +0000

    improve MealEntry styling

--- app/MealEntry.js
+++ app/MealEntry.js
@@ -1,19 +1,20 @@
 import { useEffect } from "react";
 import { View } from "react-native";
-import { Text, Title } from "react-native-paper";
+import { Divider, Text } from "react-native-paper";
 
 export default function MealEntry({ meal }) {
-  useEffect(() => {
-    console.log("Meal entry:");
-    console.log(meal);
-  }, [])
+  // useEffect(() => {
+  //   console.log("MealEntry:");
+  //   console.log(meal);
+  // }, [])
 
   return (
     <View>
-      <Text variant="titleLarge">{meal.name}</Text>
-      <Text>{meal.energy} kcal</Text>
-      <Text>{meal.grams}g</Text>
-      <Text>{meal.servings} servings</Text>
+      <Text variant="bodyLarge">{meal.name}</Text>
+      <Text >{meal.energy} kcal</Text>
+      <Text >{meal.grams}g</Text>
+      <Text >{meal.servings} servings</Text>
+      <Divider style={{ marginTop: 5 }} />
     </View>
   )
 }
