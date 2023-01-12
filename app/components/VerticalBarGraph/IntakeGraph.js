b17e620efb05f175929c796d445ce44fafbe06b6
TermoZour <stefanpruneanu@gmail.com>
2023-01-12 22:52:01 +0000

    refresh Graph when changing date

--- app/components/VerticalBarGraph/IntakeGraph.js
+++ app/components/VerticalBarGraph/IntakeGraph.js
@@ -108,7 +108,7 @@ export default function IntakeGraph({ mealData }) {
     if (mealDb != null) {
       computeMeals();
     }
-  }, [mealDb])
+  }, [mealDb, mealData])
 
   return (
     <VerticalBarGraph
