f7fec879b894189077fa6d165dde9329fba3c6b1
TermoZour <stefanpruneanu@gmail.com>
2023-01-12 15:41:58 +0000

    handle empty or wrong ID for meal entry

--- app/components/MealsView/MealEntry.js
+++ app/components/MealsView/MealEntry.js
@@ -10,23 +10,28 @@ export default function MealEntry({ meal, entry }) {
   //   console.log(entry);
   // }, [])
 
-  return (
-    <View>
-      <Text variant="bodyLarge">{meal.name}</Text>
-      {/* calories intake */}
-      {entry.isGrams ?
-        <Text>{entry.amount / meal.energy_per_100g}kcal</Text> :
-        <Text>{entry.amount * meal.energy_per_serving}kcal</Text>
-      }
+  if (meal != null)
+    return (
+      <View>
+        <Text variant="bodyLarge">{meal.name}</Text>
+        {/* calories intake */}
+        {entry.isGrams ?
+          <Text>{entry.amount / meal.energy_per_100g}kcal</Text> :
+          <Text>{entry.amount * meal.energy_per_serving}kcal</Text>
+        }
 
-      {/* grams intake */}
-      {entry.isGrams ?
-        <Text>{entry.amount}g</Text> :
-        <Text>{entry.amount} {entry.amount > 1 ? "servings" : "serving"} ({meal.grams / meal.servings * entry.amount}g)</Text>
-      }
+        {/* grams intake */}
+        {entry.isGrams ?
+          <Text>{entry.amount}g</Text> :
+          <Text>{entry.amount} {entry.amount > 1 ? "servings" : "serving"} ({meal.grams / meal.servings * entry.amount}g)</Text>
+        }
 
-      {/* <Text >{meal.servings} {meal.servings > 1 ? "servings" : "serving"}</Text> */}
-      <Divider style={{ marginTop: 5 }} />
-    </View>
-  )
+        {/* <Text >{meal.servings} {meal.servings > 1 ? "servings" : "serving"}</Text> */}
+        <Divider style={{ marginTop: 5 }} />
+      </View>
+    )
+  else
+    return (
+      <Text>Meal entry ID error</Text>
+    )
 }
